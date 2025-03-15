f99d9dfcb7854eda209e05b2b84c5c8f8a41a0ca
IcQxp <batla3080@gmail.com>
2025-03-15 18:21:36 +0300

    mui + components

--- src/auth/auth.tsx
+++ src/auth/auth.tsx
@@ -5,6 +5,14 @@ import styles from "./auth.module.scss"
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setUser } from '../store/userSlice';
+import Input from '@mui/joy/Input';
+import Stack from '@mui/joy/Stack';
+import LinearProgress from '@mui/joy/LinearProgress';
+import Typography from '@mui/joy/Typography';
+import Key from '@mui/icons-material/Key';
+import { Checkbox } from '@mui/material';
+import Button from '@mui/material/Button';
+
 
 interface JwtPayloadNew extends JwtPayload {
   unique_name: string
@@ -31,14 +39,15 @@ const AuthComponent = () => {
   const navigate = useNavigate();
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
-const dispatch = useDispatch();
+  const [isEmployee, setIsEmployee] = useState<boolean>(false);
+  const dispatch = useDispatch();
 
   const handleLogin = async () => {
     try {
       const response = await axios.post('https://localhost:7003/api/auth/login', {
         username: login,
         password: password,
-        isEmployee: false,
+        isEmployee: isEmployee,
       });
 
       if (response.data.token) {
@@ -46,11 +55,10 @@ const dispatch = useDispatch();
         localStorage.setItem('token', response.data.token);
         setError("");
       }
-      alert(response.status==200?"Успешно":"Ошибка");
-      if (response.status==200)
-      {
+      alert(response.status == 200 ? "Успешно" : "Ошибка");
+      if (response.status == 200) {
         navigate("/home");
-        
+
         dispatch(setUser(response.data.user));
       }
     } catch (err: any) {
@@ -59,7 +67,7 @@ const dispatch = useDispatch();
     }
   };
 
-  
+
 
   const GetMe = async () => {
     try {
@@ -112,33 +120,63 @@ const dispatch = useDispatch();
     }
   };
 
+
+  // const minLength = 8;
   return (
     <main className={styles.page} >
       <div className={styles.container}>
         <h2 className={styles.title}> Вход </h2>
         <div className={styles.login__container}>
-        <p className={styles.login__title}>Login</p>
-        <input placeholder='Login'
-        type='login'
-        onChange={(e) => setLogin(e.target.value)}
-        value={login}
-        className={`${styles.login__input}`} />
+          <p className={styles.login__title}>Login</p>
+          <Input color="neutral" variant="outlined" placeholder='Login'
+            type='login'
+            onChange={(e) => setLogin(e.target.value)}
+            value={login} className={styles.input}
+          // sx={{fontFamily: 'PixelizerBold'}}
+          />
         </div>
-        <div className={styles.password__container}>
-        <p className={styles.password__title}>Password</p>
-        <input  placeholder='Password'
-        type='password'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        className={`${styles.password__input}`} />
+        <div className={`${styles.password__container} ${styles.input}`}>
+          <p className={styles.password__title}>Password</p>
+          <Stack spacing={0.5} sx={{ '--hue': Math.min(password.length * 10, 120) }} >
+            <Input
+              className={styles.input}
+              type="password"
+              placeholder="Введите пароль…"
+              startDecorator={<Key />}
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            {/* <LinearProgress
+
+              determinate
+              size="sm"
+              value={Math.min((password.length * 100) / minLength, 100)}
+              sx={{ bgcolor: 'background.level3', color: 'hsl(var(--hue) 80% 40%)' }}
+            />
+            <Typography
+              level="body-xs"
+              sx={{ alignSelf: 'flex-end', color: 'hsl(var(--hue) 80% 30%)' }}
+            >
+              {password.length < 3 && 'Очень слабый'}
+              {password.length >= 3 && password.length < 6 && 'Слабый'}
+              {password.length >= 6 && password.length < 8 && 'Надёжный'}
+              {password.length >= 8 && 'Очень надёжный'}
+            </Typography> */}
+          </Stack>
+
+
+
         </div>
         <div className={`${styles.buttons__container}`} >
-          <button className={`${styles.registration__button}`}>
-            Зарегистрироваться
-          </button>
-          <button className={`${styles.auth__button}`} onClick={handleLogin}>
-            Войти
-          </button>
+          <div>
+            <label htmlFor='employeeCheckBox' className={styles.checkbox__label}>
+              Войти как администратор
+            </label>
+            <Checkbox id='employeeCheckBox' defaultChecked onChange={(e) => setIsEmployee(e.target.checked)} />
+          </div>
+
+          <Button size="small" variant="contained" onClick={handleLogin}>Войти</Button>
+
         </div>
       </div>
       <div style={{ display: 'none' }} >
